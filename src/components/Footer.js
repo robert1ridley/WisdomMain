112248267088cececa94379240a07671e8ed987a
Robert Ridley <robert.n.ridley@gmail.com>
2017-12-01 13:24:25 +0800

    footer updates

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,58 +1,45 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import { Image, Row, Col } from 'react-bootstrap';
 import footer from '../images/footer.jpg';
 import '../styles/footer.css';
+import footerdata from '../data/footerdata';
 
-export default class Footer extends React.Component {
+class Footer extends React.Component {
   render () {
+    const footerAll = this.props.language === "zh"? footerdata.chinese: footerdata.english;
+    const footerHead = footerAll.pagehead;
+    const footerItems = footerAll.footerItems.map((footerItem, idx) => 
+      <Col md={2} xs={6} key={idx}>
+        <h4 className="footer-head">{footerItem.header}</h4>
+          {footerItem.subItems.map((singleSub, index) =>
+            <p key={index}>{singleSub}</p>
+          )}
+      </Col>
+  )
+
     return (
-      <div className="wow fadeInRight">
-        {/* <div className="container" style={{padding: 40}}>
-          <h1 className="big-head text-center lower-margin">我们的客户包括</h1>
+      <div>
+        <div className="container" style={{padding: 40}}>
+          <h1 className="big-head text-center lower-margin">{footerHead.text}</h1>
           <Image className="center-img" src={footer} responsive/>
         </div>
         <div id="footer">
           <div className="container-fluid">
             <Row>
-              <Col md={2} xs={6}>
-                <h4 className="footer-head">关于智道</h4>
-                <p>公司概况</p>
-                <p>企业文化</p>
-                <p>组织架构</p>
-                <p>资质荣誉</p>
-                <p>大事记</p>
-                <p>社会责任</p>
-                <p>董事长致辞</p>
-              </Col>
-              <Col md={2} xs={6}>
-                <h4 className="footer-head">新闻中心</h4>
-                <p>企业新闻</p>
-                <p>行业动态</p>
-              </Col>
-              <Col md={2} xs={6}>
-                <h4 className="footer-head">解决方案</h4>
-                <p>主营业务</p>
-                <p>创新与研发</p>
-                <p>自主知识产权技术和装置介绍</p>
-              </Col>
-              <Col md={2} xs={6}>
-                <h4 className="footer-head">人力资源</h4>
-                <p>人才招聘</p>
-                <p>员工风采</p>
-              </Col>
-              <Col md={2} xs={6}>
-                <h4 className="footer-head">地址</h4>
-                <p>人才招聘</p>
-                <p>员工风采</p>
-              </Col>
-              <Col md={2} xs={6}>
-                <h4 className="footer-head">电话</h4>
-                <p>9400929844738</p>
-              </Col>
+              {footerItems}
             </Row>
           </div>
-        </div> */}
+        </div>
       </div>
     )
   }
 }
+
+function mapStateToProps(state) {
+  return {
+      language: state.language
+  };
+}
+
+export default connect(mapStateToProps)(Footer);
---
+++ src/data/footerdata.js
@@ -0,0 +1,107 @@
+const footerdata = {
+  chinese: {
+    pagehead: {text: "我们的客户包括", link: "/"},
+    footerItems:[
+      {
+        header: "关于智道",
+        subItems: [
+          "公司概况",
+          "企业文化",
+          "组织架构",
+          "资质荣誉",
+          "大事记",
+          "社会责任",
+          "董事长致辞"
+        ]
+      },
+      {
+        header: "新闻中心",
+        subItems: [
+          "企业新闻",
+          "行业动态"
+        ]
+      },
+      {
+        header: "解决方案",
+        subItems: [
+          "主营业务",
+          "创新与研发",
+          "自主知识产权技术和装置介绍"
+        ]
+      },
+      {
+        header: "人力资源",
+        subItems: [
+          "人才招聘",
+          "员工风采"
+        ]
+      },
+      {
+        header: "地址",
+        subItems: [
+          "南京市新模范马路5号科技创新大楼21楼B座"
+        ]
+      },
+      {
+        header: "电话",
+        subItems: [
+          "9400929844738"
+        ]
+      }
+    ]
+  },
+  english: {
+    pagehead: {text: "Our clients include", link: "/"},
+    footerItems:[
+      {
+        header: "About us",
+        subItems: [
+          "Company Profile",
+          "Corporate Culture",
+          "Organizational Structure",
+          "Achievements",
+          "Events",
+          "Social Responsibility",
+          "Chairman's Blog"
+        ]
+      },
+      {
+        header: "Company News",
+        subItems: [
+          "Industry News",
+          "Industry Trends"
+        ]
+      },
+      {
+        header: "Solutions",
+        subItems: [
+          "Main Business",
+          "Research and Development",
+          "Introduction of independent intellectual property technology and equipment"
+        ]
+      },
+      {
+        header: "Careers",
+        subItems: [
+          "Talent Recruitment",
+          "Employees"
+        ]
+      },
+      {
+        header: "Address",
+        subItems: [
+          "No.5 Xinmofan Road, Nanjing, Nanjing Sci-Tech Center, 21F-B"
+        ]
+      },
+      {
+        header: "Phone",
+        subItems: [
+          "9400929844738"
+        ]
+      }
+    ]
+  }
+}
+
+export default footerdata;
+
