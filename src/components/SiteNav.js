811c681c5e8caa0790a28d2173ca13b098497867
Robert Ridley <robert.n.ridley@gmail.com>
2017-12-02 21:50:44 +0800

    navbar updates

--- src/components/SiteNav.js
+++ src/components/SiteNav.js
@@ -2,8 +2,9 @@ import React from 'react';
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
 import {switchLanguage} from '../actions/index';
-import { Navbar, Nav, NavDropdown, MenuItem, Image } from 'react-bootstrap';
+import { Navbar, Nav, NavItem, NavDropdown, MenuItem, Image } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import { LinkContainer } from 'react-router-bootstrap';
 import headers from '../data/headersdata';
 import '../styles/sitenav.css';
 import logo from '../images/logo.png';
@@ -12,9 +13,9 @@ class SiteNav extends React.Component {
   render() {
     const headings = this.props.language === "zh"? headers.chinese: headers.english;
     const titles = headings.map(heading => 
-      <li role="presentation" key={heading.id}>
-        <Link to={heading.link}>{heading.head}</Link>
-      </li>
+      <LinkContainer to={heading.link}>
+        <NavItem eventKey={heading.id}>{heading.head}</NavItem>
+      </LinkContainer>
     )
 
     const langTitle = this.props.language === "zh"? "中文": "English";
@@ -33,8 +34,8 @@ class SiteNav extends React.Component {
           <Nav pullRight>
             {titles}
             <NavDropdown eventKey={8} title={langTitle} id="basic-nav-dropdown">
-              <MenuItem eventKey={8.1} onClick={() => this.props.switchLanguage("zh")}>中文</MenuItem>
-              <MenuItem eventKey={8.2} onClick={() => this.props.switchLanguage("en")}>English</MenuItem>
+              <MenuItem className="nav-background" eventKey={8.1} onClick={() => this.props.switchLanguage("zh")}>中文</MenuItem>
+              <MenuItem className="nav-background" eventKey={8.2} onClick={() => this.props.switchLanguage("en")}>English</MenuItem>
             </NavDropdown>
           </Nav>
         </Navbar.Collapse>
