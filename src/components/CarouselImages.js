420f62963df6efcd7f23fce2815f21098630f5ea
Robert Ridley <robert.n.ridley@gmail.com>
2017-12-03 11:23:28 +0800

    image updates

--- src/components/CarouselImages.js
+++ src/components/CarouselImages.js
@@ -4,12 +4,16 @@ import mainCarousel1 from '../images/carousel/main1.jpg';
 import mainCarousel2 from '../images/carousel/main2.jpg';
 import mainCarousel3 from '../images/carousel/main3.jpg';
 import mainCarousel4 from '../images/carousel/main4.jpg';
+import mainCarousel1tiny from '../images/carousel/main1-tiny.jpg';
+import mainCarousel2tiny from '../images/carousel/main2-tiny.jpg';
+import mainCarousel3tiny from '../images/carousel/main3-tiny.jpg';
+import mainCarousel4tiny from '../images/carousel/main4-tiny.jpg';
 
 const images = [
-  {image: mainCarousel1, id: 1},
-  {image: mainCarousel2, id: 2},
-  {image: mainCarousel3, id: 3},
-  {image: mainCarousel4, id: 4}
+  {image: mainCarousel1, blurImage: mainCarousel1tiny, id: 1},
+  {image: mainCarousel2, blurImage: mainCarousel2tiny, id: 2},
+  {image: mainCarousel3, blurImage: mainCarousel3tiny, id: 3},
+  {image: mainCarousel4, blurImage: mainCarousel4tiny, id: 4}
 ]
 
 export default class CarouselImages extends React.Component {
@@ -18,8 +22,8 @@ export default class CarouselImages extends React.Component {
     const imageData = images.map(imageDatum=>
         <Carousel.Item key={imageDatum.id}>
           <div className="wrapper">
-            <div className="img-wrapper">
-              <div className="carousel-div" style={{backgroundImage: 'url(' + imageDatum.image + ')'}} />
+            <div className="img-wrapper" style={{backgroundImage: 'url(' + imageDatum.blurImage + ')'}}>
+              <div className="carousel-div" style={{backgroundImage: 'url(' + imageDatum.image + ')'}}/>
             </div>
           </div>
         </Carousel.Item>
