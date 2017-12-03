11b5e4496ca150e22b59fa02ca7db8a2e1b58e86
Robert Ridley <robert.n.ridley@gmail.com>
2017-12-04 00:27:42 +0800

    styling updates

--- src/components/CarouselImages.js
+++ src/components/CarouselImages.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Carousel } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 import mainCarousel1 from '../images/misc/page-1_slide01.jpg';
 import mainCarousel2 from '../images/misc/page-1_slide02.jpg';
 import mainCarousel3 from '../images/misc/page-1_slide03.jpg';
@@ -8,9 +9,9 @@ import mainCarousel2tiny from '../images/misc/page-1_slide02tiny.jpg';
 import mainCarousel3tiny from '../images/misc/page-1_slide03tiny.jpg';
 
 const images = [
-  {image: mainCarousel1, blurImage: mainCarousel1tiny, id: 1, chinese: "关于智道", english: "About Wisdom"},
-  {image: mainCarousel2, blurImage: mainCarousel2tiny, id: 2, chinese: "工程业绩", english: "Our Projects"},
-  {image: mainCarousel3, blurImage: mainCarousel3tiny, id: 3, chinese: "新闻中心", english: "Company News"}
+  {image: mainCarousel1, blurImage: mainCarousel1tiny, id: 1, chinese: "关于智道", english: "About Wisdom", link: "/about"},
+  {image: mainCarousel2, blurImage: mainCarousel2tiny, id: 2, chinese: "工程业绩", english: "Our Projects", link: "/achievements"},
+  {image: mainCarousel3, blurImage: mainCarousel3tiny, id: 3, chinese: "新闻中心", english: "Company News", link: "/news"}
 ]
 
 class ImageDiv extends React.Component {
@@ -38,7 +39,9 @@ class ImageDiv extends React.Component {
     return (
       <div className={style} style={{backgroundImage: 'url(' + this.props.image + ')'}}>
         <Carousel.Caption>
-          <p>{this.props.text}</p>
+          <Link to={this.props.link}>
+            <h3>{this.props.text}</h3>
+          </Link>
         </Carousel.Caption>
       </div>
     )
@@ -54,7 +57,8 @@ export default class CarouselImages extends React.Component {
             <div className="img-wrapper" style={{backgroundImage: 'url(' + imageDatum.blurImage + ')'}}>
               <ImageDiv 
                 image={imageDatum.image}
-                text={this.props.language==="zh"? imageDatum.chinese: imageDatum.english}  
+                text={this.props.language==="zh"? imageDatum.chinese: imageDatum.english}
+                link={imageDatum.link} 
               />
             </div>
           </div>
