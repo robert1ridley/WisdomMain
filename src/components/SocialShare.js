bebf342ea25272be250362be8020ee8cf972174b
Robert Ridley <robert.n.ridley@gmail.com>
2017-12-04 19:04:38 +0800

    loading images added

--- src/components/SocialShare.js
+++ src/components/SocialShare.js
@@ -1,14 +1,32 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import backgroundImage2 from '../images/background2.jpg';
+import backgroundImage2tiny from '../images/background2-tiny.jpg';
 import Intro from '../components/Intro';
 import SocialShareBlock from '../components/SocialShareBlock';
 import sharingdata from '../data/sharingdata';
 
 class SocialShare extends React.Component {
+  constructor(props){
+    super(props);
+
+    this.componentDidMount = this.componentDidMount.bind(this)
+    this.state = {
+      visible: false
+    }
+  }
+
+  show(){
+      this.setState({visible: true})
+  }
+
+  componentDidMount(){
+      this.show()
+  }
+
   render () {
     return (
-      <div style={styles.background}>
+      <div style={this.state.visible===false? styles.loading: styles.background}>
         <div className="container">
           <Intro 
             data={sharingdata}
@@ -25,6 +43,10 @@ class SocialShare extends React.Component {
 const styles = {
   background: {
     background: 'url(' + backgroundImage2 + ') no-repeat center center fixed',
+    minHeight: 565
+  },
+  loading: {
+    background: 'url(' + backgroundImage2tiny + ') no-repeat center center fixed',
     minHeight: 565
   }
 }
--- src/components/Staff.js
+++ src/components/Staff.js
@@ -1,13 +1,31 @@
 import React from 'react';
 import backgroundImage from '../images/background1.jpg';
+import backgroundImagetiny from '../images/background1-tiny.jpg';
 import Intro from './Intro';
 import Cards from './Cards';
 
 
 export default class Staff extends React.Component {
+  constructor(props){
+    super(props);
+
+    this.componentDidMount = this.componentDidMount.bind(this)
+    this.state = {
+      visible: false
+    }
+  }
+
+  show(){
+      this.setState({visible: true})
+  }
+
+  componentDidMount(){
+      this.show()
+  }
+
   render () {
     return (
-      <div style={styles.background}>
+      <div style={this.state.visible===false? styles.loading: styles.background}>
         <div className="container">
           <Intro 
             language={this.props.language}
@@ -24,6 +42,11 @@ export default class Staff extends React.Component {
 const styles = {
   background: {
     background: 'url(' + backgroundImage + ') no-repeat center center fixed',
+    minHeight: 565
+  },
+
+  loading: {
+    background: 'url(' + backgroundImagetiny + ') no-repeat center center fixed',
     minHeight: 565
   }
 }
