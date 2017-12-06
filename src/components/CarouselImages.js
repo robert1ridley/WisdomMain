b92482a72cff743da9a65a55e6ffc7b46b224c50
Robert Ridley <robert.n.ridley@gmail.com>
2017-12-06 12:05:22 +0800

    style updates

--- src/components/CarouselImages.js
+++ src/components/CarouselImages.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Carousel } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
-import mainCarousel1 from '../images/misc/page-1_slide01.jpg';
-import mainCarousel2 from '../images/misc/page-1_slide02.jpg';
-import mainCarousel3 from '../images/misc/page-1_slide03.jpg';
-import mainCarousel1tiny from '../images/misc/page-1_slide01tiny.jpg';
-import mainCarousel2tiny from '../images/misc/page-1_slide02tiny.jpg';
-import mainCarousel3tiny from '../images/misc/page-1_slide03tiny.jpg';
+import mainCarousel1 from '../images/carousel/test/page-1_slide01.jpg';
+import mainCarousel2 from '../images/carousel/test/page-1_slide02.jpg';
+import mainCarousel3 from '../images/carousel/test/page-1_slide03.jpg';
+import mainCarousel1tiny from '../images/carousel/test/page-1_slide01tiny.jpg';
+import mainCarousel2tiny from '../images/carousel/test/page-1_slide02tiny.jpg';
+import mainCarousel3tiny from '../images/carousel/test/page-1_slide03tiny.jpg';
 
 const images = [
   {image: mainCarousel1, blurImage: mainCarousel1tiny, id: 1, chinese: "关于智道", english: "About Wisdom", link: "/about"},
@@ -54,7 +54,8 @@ export default class CarouselImages extends React.Component {
     const imageData = images.map(imageDatum=>
         <Carousel.Item key={imageDatum.id}>
           <div className="wrapper">
-            <div className="img-wrapper" style={{backgroundImage: 'url(' + imageDatum.blurImage + ')'}}>
+            <div className="img-wrapper" style={{backgroundImage:'url(' + imageDatum.blurImage + ')'}}
+              >
               <ImageDiv 
                 image={imageDatum.image}
                 text={this.props.language==="zh"? imageDatum.chinese: imageDatum.english}
