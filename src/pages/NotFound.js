6e3a2d4ce1942b2fb74982e060c1afaea6ece088
Robert Ridley <robert.n.ridley@gmail.com>
2018-05-03 13:12:23 +0800

    updates to 404 page

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,11 +1,67 @@
 import React from 'react';
+import { Button } from 'react-bootstrap';
+import { withRouter } from 'react-router-dom';
+import {connect} from 'react-redux';
 
-export default class NotFound extends React.Component {
+const text = {
+  head: {
+    zh: "发生错误",
+    en: "Oops! Something went wrong"
+  },
+  body: {
+    zh: "或许这个网页不存在了",
+    en: "Perhaps the webpage doesn't exist any longer!"
+  },
+  button: {
+    zh: "返回",
+    en: "Go back"
+  }
+}
+
+class NotFound extends React.Component {
+
+  goBack = (props) => {
+    this.props.history.goBack();
+  }
+
   render() {
+    const { language } = this.props;
     return (
-      <div>
-        <h1>Page Not Found</h1>
+      <div className="text-center central-info wow fadeIn">
+        <h1 className="big-head">{text.head[language]}</h1>
+        <p className="big-body">{text.body[language]}</p>
+        <Button 
+          onClick={this.goBack.bind(this)}
+          className="rounded-card"
+          style={styles.buttonStyle}
+        >
+        {text.button[language]}
+        </Button>
       </div>
     )
   }
 }
+
+const styles = {
+  buttonStyle: {
+    marginTop: 20,
+    borderRadius: 0,
+    paddingLeft: 20,
+    paddingRight: 20,
+    paddingTop: 10,
+    paddingBottom: 10,
+    fontSize: 24,
+    fontWeight: 100,
+    backgroundColor: '#7ebc59',
+    color: 'white'
+  }
+}
+
+function mapStateToProps(state) {
+  return {
+      language: state.language
+  };
+}
+
+const wrappedComponent = connect(mapStateToProps)(NotFound)
+export default withRouter(wrappedComponent);
