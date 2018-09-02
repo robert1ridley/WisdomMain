86c8e6af60271c38ef4f556c0b107ee3b2a4aee7
Robert Ridley <robert.n.ridley@gmail.com>
2018-09-02 17:59:39 +0800

    undo share

--- src/components/SocialShareBlock.js
+++ src/components/SocialShareBlock.js
@@ -2,30 +2,15 @@ import React from 'react';
 import { Row, Col } from 'react-bootstrap';
 
 export default class SocialShareBlock extends React.Component {
-  componentDidMount () {
-    var script = document.createElement('script');
-    script.src = "http://bdimg.share.baidu.com/static/api/js/share.js?cdnversion=408841";
-    document.body.appendChild(script);
-    
-}
-
   render () {
     return (
-      // <Row className="central-info wow fadeInLeft">
-      //   <Col md={3} sm={3} />
-      //   <SocialItem img="fa fa-facebook fa-3x circle-child"/>
-      //   <SocialItem img="fa fa-weixin fa-3x circle-child"/>
-      //   <SocialItem img="fa fa-weibo fa-3x circle-child"/>
-      //   <Col md={3} sm={4} />
-      // </Row>
-      <div className="comment">
-        <div class="list-main-left-hover bdsharebuttonbox" data-tag="share_1">
-            <span class="list-main-left-hovertit" href="javascript:;"></span>
-            <a class="bds_weixin" data-cmd="weixin" href="javascript:;">微信朋友圈</a>
-            <a class="bds_tsina" data-cmd="tsina" href="javascript:;">QQ空间</a>
-            <a class="bds_qzone" data-cmd="qzone" href="javascript:;">微博</a>
-        </div>
-      </div>
+      <Row className="central-info wow fadeInLeft">
+        <Col md={3} sm={3} />
+        <SocialItem img="fa fa-facebook fa-3x circle-child"/>
+        <SocialItem img="fa fa-weixin fa-3x circle-child"/>
+        <SocialItem img="fa fa-weibo fa-3x circle-child"/>
+        <Col md={3} sm={4} />
+      </Row>
     )
   }
 }
--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import {createLogger} from 'redux-logger';
 import allReducers from './reducers';
 import App from './App';
 
-// import 'animate.css';
+import 'animate.css';
 import './libs/bootstrap-3.3.6/dist/css/bootstrap.min.css';
 import './libs/font-awesome/css/font-awesome.min.css';
 import './index.css';
