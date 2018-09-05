40600c19fa1c5c1242fbc26dbe799e80bbd10531
Robert Ridley <robert.n.ridley@gmail.com>
2018-09-05 15:02:55 +0800

    update to mail

--- server/mail/index.js
+++ server/mail/index.js
@@ -16,7 +16,7 @@ exports.consultEmail = function(name, email, emailMessage) {
       
   let message = {
       from: `${name} <${process.env.MAIL_CLIENT}>`,
-      to: `Wisdom Employee <${process.env.MAIL_CLIENT}>`,
+      to: `江苏智道 <${process.env.MAIL_CLIENT}>`,
       subject: `${name} – 咨询`,
       html: `<p>${emailMessage}</p></br/><p style="color: #6a6f7c">发信息人的邮件: ${email}</p>`
   };
