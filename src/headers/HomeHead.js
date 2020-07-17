907424746457339b6b8b7255b816b1c72527c697
Robert Ridley <robert.n.ridley@gmail.com>
2020-07-17 15:05:46 +0800

    add some stuff for seo

--- src/headers/HomeHead.js
+++ src/headers/HomeHead.js
@@ -5,7 +5,7 @@ const HomeHead = (props) => {
   return (
     <Helmet>
       <meta charSet="utf-8" />
-      <title>智道</title>
+      <title>江苏智道工程技术有限公司</title>
       <meta name="keywords" content="江苏智道工程技术有限公司|油气回收、VOCs治理技术；脱硫脱硝技术；能量综合利用；膜；分子筛膜；中空纤维分子筛膜|江苏智道；石油化工；焦化；油气；VOCs；罐区；低温吸收；吸附；氧化；多级冷凝；直冷；蓄冷；油品；化工产品 ；装车；装船" />
       <meta name="description" content="江苏智道, 工程技术有限公司, 是集研发, 设计, 核心制造, 工程总承包为一体, 专注, 于工业, 企业系统, 能量, 综合利用, 有效成分回收, 处理达标排放的国家级高新技术企业, 节能环保系统优化解决的专家" />
     </Helmet>
