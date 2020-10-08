4babac74c6ef5f9582be88c4a0f9258f9f524187
Robert Ridley <robert.n.ridley@gmail.com>
2020-10-08 17:06:05 +0800

    add multiple email recipients

--- server/models/mailModels/ConsultMessage.js
+++ server/models/mailModels/ConsultMessage.js
@@ -1,8 +1,8 @@
 exports.ConsultMessage = (name, email, phonenumber, message) => {
   const finalMessage = {
-    from: process.env.MAIL_CLIENT,
+    from: `${process.env.MAIL_CLIENT}, ${process.env.MAIL_CLIENT_TWO}`,
     to: process.env.MAIL_CLIENT,
-    subject: `${name} - 从网站发的`,
+    subject: `${name} - 从江苏智道网站发的`,
     html: `<p>姓名：${name}</p><p>邮箱：${email}</p><p>电话：${phonenumber}</p><p>${message}</p>`,
     replyto: email
   }
