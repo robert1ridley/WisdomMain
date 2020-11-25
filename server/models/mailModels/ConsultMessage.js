b88f33ba6c5cd9e6276526551b940e7f8d4fd4c1
Robert Ridley <robert.n.ridley@gmail.com>
2020-11-25 17:50:56 +0800

    add third email address

--- server/models/mailModels/ConsultMessage.js
+++ server/models/mailModels/ConsultMessage.js
@@ -1,7 +1,7 @@
 exports.ConsultMessage = (name, email, phonenumber, message) => {
   const finalMessage = {
     from: process.env.MAIL_CLIENT,
-    to: `${process.env.MAIL_CLIENT}, ${process.env.MAIL_CLIENT_TWO}`,
+    to: `${process.env.MAIL_CLIENT}, ${process.env.MAIL_CLIENT_TWO}, ${process.env.MAIL_CLIENT_THREE}`,
     subject: `${name} - 从江苏智道网站发的`,
     html: `<p>姓名：${name}</p><p>邮箱：${email}</p><p>电话：${phonenumber}</p><p>${message}</p>`,
     replyto: email
